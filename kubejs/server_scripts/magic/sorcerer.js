71fd67d3651560cf6a60c8b69238d3ad0be4fe84
cloby <76920325+cloby0@users.noreply.github.com>
2026-05-04 10:52:37 -0400

    lets you use strong essences in place of weak ojnes

--- kubejs/server_scripts/magic/sorcerer.js
+++ kubejs/server_scripts/magic/sorcerer.js
@@ -10,13 +10,24 @@ ServerEvents.recipes(event => {
         event.recipes.ars_nouveau.enchanting_apparatus(
             [
                 `ars_nouveau:${element}_essence`,
+                `mysticalagriculture:${element}_agglomeratio`,
                 `ars_nouveau:${element}_essence`,
+                `mysticalagriculture:${element}_agglomeratio`,
                 `ars_nouveau:${element}_essence`,
+                `mysticalagriculture:${element}_agglomeratio`,
                 `ars_nouveau:${element}_essence`,
+                `mysticalagriculture:${element}_agglomeratio`
             ],
-            "minecraft:gunpowder",
+            "ars_nouveau:magebloom_crop",
             `mysticalagriculture:${element}_seeds`,
             1000,
         );
+        event.replaceInput(
+            { input: `ars_nouveau:${element}_essence` },
+            `ars_nouveau:${element}_essence`,
+            `#kubejs:${element}_essences`
+        )
     })
+
+
 });
--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,3 +1,15 @@
 ServerEvents.tags('item', event => {
   event.add('aether:aether_portal_activation_items', 'gtceu:luminessence_dust');
+
+  const elements = [
+    "air",
+    "earth",
+    "fire",
+    "water"
+  ]
+
+  elements.forEach(element => {
+    event.add(`kubejs:${element}_essences`, `ars_nouveau:${element}_essence`);
+    event.add(`kubejs:${element}_essences`, `mysticalagriculture:${element}_essence`);
+  })
 });
