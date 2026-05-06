92b7cfb632d5fd1b7d1ce16d8ab854f7a1dda69a
cloby <76920325+cloby0@users.noreply.github.com>
2026-05-06 11:38:33 -0400

    maybe fixes the aether worldgen?

--- kubejs/startup_scripts/oreRegistry.js
+++ kubejs/startup_scripts/oreRegistry.js
@@ -1,8 +1,29 @@
-GTCEuStartupEvents.registry('gtceu:tag_prefix', event => {
-    event.create('holystone', 'ore') // 
-        .stateSupplier(() => Block.getBlock('aether:holystone').defaultBlockState()) // 
-        .baseModelLocation('aether:block/holystone') // 
-        .unificationEnabled(true)
-        .materialIconType(GTMaterialIconType.ore)
-        .generationCondition(ItemGenerationCondition.hasOreProperty)
+let UtilsJS = Java.loadClass("dev.latvian.mods.kubejs.util.UtilsJS")
+
+GTCEuStartupEvents.registry('gtceu:tag_prefix', e => {
+    function createStoneTypeOre(type, properties) {
+        if (properties === undefined)
+            properties = {}
+
+        if (properties.baseModel === undefined)
+            properties.baseModel = type.namespace + ":block/" + type.path
+
+        if (properties.material === undefined)
+            properties.material = null
+
+        if (properties.blockState === undefined)
+            properties.blockState = (() => Block.getBlock(type).defaultBlockState())
+
+        e.create(type.path, 'ore')
+            .stateSupplier(properties.blockState)
+            .baseModelLocation(properties.baseModel)
+            .materialSupplier(properties.material)
+            .unificationEnabled(true)
+            .materialIconType(GTMaterialIconType.ore)
+            .miningToolTag("mineable/pickaxe")
+            .generationCondition(ItemGenerationCondition.hasOreProperty)
+    }
+    
+    createStoneTypeOre('aether:holystone', { blockState: () => 
+        UtilsJS.parseBlockState("aether:holystone[double_drops=true]")})
 })
