a4ba91f684a3bcd86dcda5fa70dfe35a02addbd2
cloby <76920325+cloby0@users.noreply.github.com>
2026-05-08 19:06:52 -0400

    commenting this out so that the commit is stable

--- kubejs/startup_scripts/customMachines/tech/arms_manufacturer.js
+++ kubejs/startup_scripts/customMachines/tech/arms_manufacturer.js
@@ -1,3 +1,4 @@
+/*
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('arms_making')
         .category('ctech')
@@ -10,8 +11,16 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
 
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
-    event.create('arms_manufacturer', 'simple', 0, GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV, GTValues.ZPM, GTValues.UV,) // 
+    event.create('arms_manufacturer', 'simple')
+        .tiers(GTValues.LV, GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV, GTValues.ZPM, GTValues.UV,)
         .rotationState(RotationState.NON_Y_AXIS)
         .recipeType('arms_making')
         .tankScalingFunction(tier => tier * 3200)
+
+        .definition((tier, builder) =>
+            builder
+                .langValue(GTValues.VLVH[tier] + " Arms Manufacturer")
+                .recipeType('arms_making')
+                .workableTieredHullModel('gtceu:block/machines/assembler'))
 })
+*/
