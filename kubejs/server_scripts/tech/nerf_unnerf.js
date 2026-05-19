cb26124066ef266723ff6fc07f1aef99c3ae8938
cloby <76920325+cloby0@users.noreply.github.com>
2026-05-19 17:17:32 -0400

    removes those bloat tacz mods bc we dont REALLY need them, and also adds some nerfs

--- kubejs/server_scripts/tech/nerf_unnerf.js
+++ kubejs/server_scripts/tech/nerf_unnerf.js
@@ -3,9 +3,26 @@
 //be craftable by magic players, with a slight amount of difficulty
 
 ServerEvents.recipes(event => {
-    //nerf
+    //this is just removing recipes that get around nerfs
     event.remove({ id: 'terralith:piston_alt' });
     event.remove({ id: 'aether:skyroot_piston' });
+    event.remove({ id: 'aether:gravitite_jukebox' });
+    event.remove({ id: 'aether:skyroot_jukebox' });
+    event.remove({ id: 'aether:skyroot_gravitite_jukebox' });
+    event.remove({ id: 'aether:skyroot_crafting_table' });
+    event.remove({ id: 'deep_aether:skyroot_crafting_table' });
+    event.remove({ id: 'aether:skyroot_cartography_table' });
+    event.remove({ id: 'aether:skyroot_fletching_table' });
+    event.remove({ id: 'aether:skyroot_smithing_table' });
+    event.remove({ id: 'aether:skyroot_grindstone' });
+    event.remove({ id: 'aether:skyroot_grindstone_holystone_slab' });
+    event.remove({ id: 'aether:skyroot_loom' });
+    event.remove({ id: 'aether:skyroot_noteblock' });
+    event.remove({ id: 'aether:skyroot_gravitite_jukebox' });
+    event.remove({ id: 'aether:skyroot_beehive' });
+    event.remove({ id: 'aether:skyroot_crafting_table' });
+    event.remove({ id: 'supplementaries:bed_from_feather_block' });
+
 
     //unnerf
     event.custom(
