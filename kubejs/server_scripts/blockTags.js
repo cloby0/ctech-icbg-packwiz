a398080874953590200d396e2d531ea325a8d1c4
cloby <76920325+cloby0@users.noreply.github.com>
2026-05-20 19:10:07 -0400

    i fucked it up so we're gonna try again

--- kubejs/server_scripts/blockTags.js
+++ kubejs/server_scripts/blockTags.js
@@ -1,4 +1,4 @@
 
 ServerEvents.tags('block', event => {
-    event.add('forge:needs_wooden_tool', '#forge:needs_wood_tool')
+    event.add('minecraft:needs_wooden_tool', '#forge:needs_wood_tool')
 })
