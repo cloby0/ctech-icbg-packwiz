7c0ce638f4fa3c2069a6cbebba1d61bdb1938cc8
cloby <76920325+cloby0@users.noreply.github.com>
2026-06-03 12:26:35 -0400

    fixes epitaxial growth chamber maybe

--- kubejs/startup_scripts/customMachines/tech/epitaxial_growth_chamber.js
+++ kubejs/startup_scripts/customMachines/tech/epitaxial_growth_chamber.js
@@ -5,7 +5,7 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         .setMaxIOSize(2, 2, 2, 0)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.ASSEMBLER);
-});
+})
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('epitaxial_growth_chamber', 'multiblock')
@@ -13,29 +13,31 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
         .rotationState(RotationState.NON_Y_AXIS)
         .appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
         .recipeTypes(['epitaxial_growth_chamber'])
-        .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH, GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK)])
+        .recipeModifiers([
+            GTRecipeModifiers.PARALLEL_HATCH,
+            GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK)
+        ])
         .pattern(definition => FactoryBlockPattern.start()
-            .aisle("abbba", "ccccc", "ccccc", "ccccc", "abbba")
-            .aisle("bdddb", "cdbdc", "cdbdc", "cdbdc", "bdddb")
-            .aisle("bdddb", "cdddc", "cdedc", "cdddc", "bdddb")
-            .aisle("bdddb", "cdddc", "cdedc", "cdddc", "bdddb")
-            .aisle("bdddb", "cdddc", "cfegc", "cdddc", "bdddb")
-            .aisle("bdddb", "cdddc", "cdedc", "cdddc", "bdddb")
-            .aisle("bdddb", "cdddc", "cdedc", "cdddc", "bdddb")
-            .aisle("bdddb", "cdbdc", "cdbdc", "cdbdc", "bdddb")
-            .aisle("abbba", "ccccc", "ccccc", "ccccc", "abbba")
-
-            .where("a", Predicates.blocks("gtceu:solid_machine_casing"))
-            .where("b", Predicates.blocks("gtceu:yellow_stripes_block_a"))
-            .where("c", Predicates.any())
-            .where("d", Predicates.blocks("gtceu:solid_machine_casing"))
+            .aisle("bbbbbb", "bddddb", "bdbbbb", "bdbaaa", "bbbaaa")
+            .aisle("bbbbdb", "beeeed", "bebbfb", "bebaaa", "bfbaaa")
+            .aisle("bbbbdb", "bgbded", "bgbbfb", "bgbaaa", "bbbaaa")
+            .aisle("aaabdb", "aaaded", "aaabfb", "aaaaaa", "aaaaaa")
+            .aisle("aaabdb", "aaaded", "aaabfb", "aaaaaa", "aaaaaa")
+            .aisle("aaabdb", "aaaded", "aaabfb", "aaaaaa", "aaaaaa")
+            .aisle("aaabdb", "aaaded", "aaabfb", "aaaaaa", "aaaaaa")
+            .aisle("aaabdb", "aaaded", "aaabfb", "aaaaaa", "aaaaaa")
+            .aisle("aaabbb", "aaabcb", "aaabbb", "aaaaaa", "aaaaaa")
+            .where("a", Predicates.any())
+            .where("b", Predicates.blocks("gtceu:solid_machine_casing"))
+            .where("c", Predicates.controller(Predicates.blocks(definition.get())))
+            .where("d", Predicates.blocks("gtceu:yellow_stripes_block_a")
                 .or(Predicates.autoAbilities(definition.getRecipeTypes()))
-                .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1))
-            .where("e", Predicates.blocks("gtceu:steel_gearbox"))
-            .where("f", Predicates.abilities(PartAbility.MAINTENANCE))
-            .where("g", Predicates.controller(Predicates.blocks(definition.get())))
-
-        .build())
+                .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1)))
+            .where("e", Predicates.blocks("gtceu:steel_pipe_casing"))
+            .where("f", Predicates.blocks("gtceu:assembly_line_grating"))
+            .where("g", Predicates.blocks("gtceu:tempered_glass"))
+            .build()
+        )
         .workableCasingModel(
             "gtceu:block/solid_machine_casing",
             "gtceu:block/multiblock/large_chemical_reactor"
