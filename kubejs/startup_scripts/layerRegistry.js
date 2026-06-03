50d4505f23d1135d73f1898328833107de170f33
cloby <76920325+cloby0@users.noreply.github.com>
2026-06-03 15:12:47 -0400

    adds a dimension marker to the moon

--- kubejs/startup_scripts/layerRegistry.js
+++ kubejs/startup_scripts/layerRegistry.js
@@ -1,3 +1,10 @@
+GTCEuStartupEvents.registry('gtceu:dimension_marker', event => {
+        event.create('ad_astra:moon')
+        .iconSupplier(() => Item.of('ad_astra:mars_globe').getItem())
+        .tier(0)
+        .overrideName('Moon')
+})
+
 GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
     event.create('holystone')
         .targets('#aether:holystone') 
