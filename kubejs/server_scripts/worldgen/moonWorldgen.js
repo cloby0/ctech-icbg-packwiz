96228ffd2ebe34f5c68bbf568a2b4736b67c1e0b
cloby <76920325+cloby0@users.noreply.github.com>
2026-06-04 15:43:35 -0400

    add some more bullshit onto the moon

--- kubejs/server_scripts/worldgen/moonWorldgen.js
+++ kubejs/server_scripts/worldgen/moonWorldgen.js
@@ -1,4 +1,19 @@
 GTCEuServerEvents.oreVeins(event => {
+    event.modify("gtceu:magnetite_vein_end", vein => {
+        vein.dimensions("ad_astra:moon")
+        vein.layer("moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+    })
+    event.modify("gtceu:bauxite_vein_end", vein => {
+        vein.dimensions("ad_astra:moon")
+        vein.layer("moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+    })
+    event.modify("gtceu:sheldonite_vein", vein => {
+        vein.dimensions("ad_astra:moon")
+        vein.layer('moon_stone')
+        vein.biomes('ad_astra:lunar_wastelands')
+    })
 //Desh
     event.add("kubejs:desh_vein_mn", vein => {
     // Basic vein generation properties
@@ -28,6 +43,82 @@ GTCEuServerEvents.oreVeins(event => {
             .radius(5)
         ) 
     })
+//Molybdenum
+    event.add("kubejs:molybdenum_vein_mn", vein => {
+        vein.weight(5)
+        vein.clusterSize(27)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(20, 50)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Wulfenite).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Molybdenite).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Molybdenum).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Powellite).size(1, 1))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Molybdenum)
+            .placement("surface")
+            .density(0.4)
+            .radius(5)
+        ) 
+    })
+
+//Quartzite
+    event.add("kubejs:quartz_vein_mn", vein => {
+        vein.weight(20)
+        vein.clusterSize(29)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(40, 80)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Quartzite).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.CertusQuartz).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Barite).size(1, 1))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Quartzite)
+            .placement("surface")
+            .density(0.4)
+            .radius(5)
+        ) 
+    })
+
+//Monazite
+    event.add("kubejs:monazite_vein_mn", vein => {
+        vein.weight(30)
+        vein.clusterSize(36)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(20, 40)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Bastnasite).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Monazite).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Neodymium).size(1, 1))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Monazite)
+            .placement("surface")
+            .density(0.4)
+            .radius(5)
+        ) 
+    })
+
 //Ilmenite
     event.add("kubejs:ilmenite_vein_mn", vein => {
         vein.weight(30)
@@ -53,4 +144,105 @@ GTCEuServerEvents.oreVeins(event => {
             .radius(5)
         ) 
     })
+
+//Copper
+    event.add("kubejs:copper_vein_mn", vein => {
+        vein.weight(80)
+        vein.clusterSize(40)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(-40, 10)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Chalcopyrite).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Iron).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Pyrite).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Copper).size(1, 1))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Copper)
+            .placement("surface")
+            .density(0.4)
+            .radius(5)
+        ) 
+    })
+
+//Galena
+    event.add("kubejs:galena_vein_mn", vein => {
+        vein.weight(40)
+        vein.clusterSize(36)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(-5, 45)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Galena).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Silver).size(1, 1))
+                .layer(l => l.weight(1).mat(GTMaterials.Lead).size(1, 1))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Galena)
+            .placement("surface")
+            .density(0.4)
+            .radius(5)
+        ) 
+    })
+
+//Cassiterite
+    event.add("kubejs:cassiterite_vein_mn", vein => {
+        vein.weight(50)
+        vein.clusterSize(44)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(60, 220)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Tin).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Cassiterite).size(1, 1))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Cassiterite)
+            .placement("surface")
+            .density(0.4)
+            .radius(5)
+        )
+    })
+
+//Iron
+    event.add("kubejs:iron_vein_mn", vein => {
+        vein.weight(80)
+        vein.clusterSize(44)
+        vein.density(0.8)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("moon")
+        vein.dimensions("ad_astra:moon")
+        vein.biomes("ad_astra:lunar_wastelands")
+        vein.heightRangeUniform(20, 60)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(5).mat(GTMaterials.Goethite).size(8, 16))
+                .layer(l => l.weight(2).mat(GTMaterials.YellowLimonite).size(8, 12))
+                .layer(l => l.weight(2).mat(GTMaterials.Hematite).size(8, 8))
+                .layer(l => l.weight(1).mat(GTMaterials.Malachite).size(8, 8))
+            )
+        )
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Hematite)
+            .placement("surface")
+            .density(0.2)
+            .radius(5))
+    })
+
 })
